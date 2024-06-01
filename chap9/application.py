28af3137e79deb1530307958d5ac88fff270dd47
minwy12 <owen.min@daangn.com>
2024-06-02 00:42:22 +0900

    feat: chap9

---
+++ chap9/application.py
@@ -0,0 +1,64 @@
+from flask import Flask, render_template, request
+import sys
+from chatbot import Chatbot
+from common import model
+from characters import system_role, instruction
+from parallel_function_calling import FunctionCalling, tools
+
+# jjinchin 인스턴스 생성
+jjinchin = Chatbot(model=model.basic, system_role=system_role, instruction=instruction)
+func_calling = FunctionCalling(model=model.basic)
+application = Flask(__name__)
+
+
+@application.route("/")
+def hello():
+    return "Hello world!"
+
+
+@application.route("/welcome")
+def welcome():  # 함수명은 꼭 welcome일 필요는 없습니다.
+    return "Welcome!"
+
+
+@application.route("/chat-app")
+def chat_app():
+    return render_template("chat.html")
+
+
+# 메아리 코드
+# @application.route('/chat-api', methods=['POST'])
+# def chat_api():
+#     print("request.json:", request.json)
+#     return {"response_message": "나도 " + request.json['request_message']}
+
+
+@application.route("/chat-api", methods=["POST"])
+def chat_api():
+    request_message = request.json["request_message"]
+    print("request_message:", request_message)
+    jjinchin.add_user_message(request_message)
+
+    # ChatGPT에게 함수 사양을 토대로 사용자 메시지에 호응하는 함수 정보를 분석해달라고 요청
+    analyzed_message, analyzed_dict = func_calling.analyze(request_message, tools)
+    # ChatGPT가 함수 호출이 필요하다고 분석했는지 여부 체크
+    if analyzed_dict.get("tool_calls"):
+        # ChatGPT가 분석해준대로 함수 호출
+        response = func_calling.run(
+            analyzed_message, analyzed_dict, jjinchin.context[:]
+        )
+    else:
+        response = jjinchin.send_request()
+
+    print(response)
+    jjinchin.add_response(response)
+    response_message = jjinchin.get_response_content()
+    jjinchin.handle_token_limit(response)
+    jjinchin.clear_instructions()
+    print("response_message:", response_message)
+
+    return {"response_message": response_message}
+
+
+if __name__ == "__main__":
+    application.run(host="0.0.0.0", port=int(sys.argv[1]))
---
+++ chap9/chatbot.py
@@ -0,0 +1,109 @@
+from common import client, model, makeup_response
+from pprint import pprint
+import math
+
+
+class Chatbot:
+    def __init__(self, model, system_role, instruction):
+        self.context = [{"role": "system", "content": system_role}]
+        self.model = model
+        self.instruction = instruction
+        self.max_token_size = 16 * 1024
+        self.available_token_rate = 0.9
+
+    def add_user_message(self, message):
+        self.context.append({"role": "user", "content": message})
+
+    def send_request(self):
+        try:
+            self.context[-1]["content"] += self.instruction
+            print("@@CONTEXT@@")
+            print(self.context)
+
+            response = client.chat.completions.create(
+                model=self.model,
+                messages=self.context,
+                temperature=0.5,
+                top_p=1,
+                max_tokens=256,
+                frequency_penalty=0,
+                presence_penalty=0,
+            ).model_dump()
+        except Exception as e:
+            print(f"Exception 오류({type(e)}) 발생: {e}")
+            if isinstance(
+                e, openai.error.InvalidRequestError
+            ) and "maximum context length" in str(e):
+                self.context.pop()
+                return makeup_response("메시지 좀 짧게 보내줄래?")
+            else:
+                return makeup_response(
+                    "챗봇에 문제가 발생했습니다. 잠시 후 다시 시도해주세요."
+                )
+
+        return response
+
+    def add_response(self, response):
+        self.context.append(
+            {
+                "role": response["choices"][0]["message"]["role"],
+                "content": response["choices"][0]["message"]["content"],
+            }
+        )
+
+    def get_response_content(self):
+        return self.context[-1]["content"]
+
+    def clear_instructions(self):
+        for idx in reversed(range(len(self.context))):
+            if self.context[idx]["role"] == "user":
+                self.context[idx]["content"] = (
+                    self.context[idx]["content"].split("instruction:\n")[0].strip()
+                )
+                break
+
+    def handle_token_limit(self, response):
+        try:
+            curr_usage_rate = response["usage"]["total_tokens"] / self.max_token_size
+            # print("curr usage rate:", curr_usage_rate)
+
+            if curr_usage_rate > self.available_token_rate:
+                remove_size = math.ceil(len(self.context) / 10)
+                self.context = [self.context[0]] + self.context[remove_size + 1 :]
+
+        except Exception as e:
+            print(f"handle_token_limit exception: {e}")
+
+
+if __name__ == "__main__":
+    # step-3: 테스트 시나리오에 따라 실행 코드 작성 및 예상 출력결과 작성
+    chatbot = Chatbot(model.basic)
+
+    # chatbot.add_user_message("Who won the world series in 2020?")
+    chatbot.add_user_message("Who won the NBA final in 2021?")
+
+    # 시나리오1-4: 현재 context를 openai api 입력값으로 설정하여 전송
+    response = chatbot.send_request()
+
+    # 시나리오1-5: 응답 메시지를 context에 추가
+    chatbot.add_response(response)
+
+    # 시나리오1-7: 응답 메시지 출력
+    print(chatbot.get_response_content())
+    print()
+
+    # 시나리오2-2: 사용자가 채팅창에 "Where was it played?" 입력
+    # chatbot.add_user_message("Where was it played?")
+    chatbot.add_user_message("Where was it played?")
+
+    # 다시 요청 보내기
+    response = chatbot.send_request()
+
+    # 응답 메시지를 context에 추가
+    chatbot.add_response(response)
+
+    # 응답 메시지 출력
+    print(chatbot.get_response_content())
+    print()
+    print("-----------------")
+    pprint(chatbot.context)
